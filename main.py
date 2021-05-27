17704a7ce718918f32acd7dcf9d076b116c400e8
ShinAKS <ayushsinha0@gmail.com>
2021-05-27 21:25:12 +0530

    Updated working code for alert

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
 import json
 
-def alert():
+#
+def alert(comments,score):
+    score = score.split()[0]
+    comment = comments['ballComments'][0]
+    over = comment['oversActual']
+    action = comment['title']
+    isOut = comment['isWicket']
+    runs = comment['totalRuns']
+    print(', '.join([str(over),str(score),action,'OUT' if isOut else  "{} runs".format(runs)]))
+
+def getData():
     uri = "https://hs-consumer-api.espncricinfo.com/v1/pages/match/details?lang=en&seriesId=1244186&matchId=1244274&latest=true"
     response = requests.get(uri)
     json_data = json.loads(response.text)
 
-    print(json_data['recentBallCommentary']['ballComments'][0])
-
+    # print(json_data['recentBallCommentary'],json_data['liveSummary']['fowText'])
+    alert(json_data['recentBallCommentary'],json_data['liveSummary']['fowText'])
 if __name__=="__main__":
-    alert()
+    getData()
